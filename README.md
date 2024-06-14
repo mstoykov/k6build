feda9034c95b74c2781ffc0d26fdff88a0e46da6
Pablo Chacin <pablochacin@gmail.com>
2024-06-14 15:17:30 +0200

    implement command
    Signed-off-by: Pablo Chacin <pablochacin@gmail.com>

---
+++ README.md
@@ -0,0 +1,45 @@
+# k6 build service
+
+
+## local build service
+
+```
+k6 build service returns artifacts that satisfies certain dependencies
+
+Usage:
+  k6build [flags]
+
+Flags:
+  -c, --catalog string           dependencies catalog (default "catalog.json")
+  -d, --dependency stringArray   list of dependencies in form package:constrains
+  -h, --help                     help for k6build
+  -k, --k6-constrains string     k6 version constrains (default "*")
+  -p, --platform string          target platform
+  -v, --verbose                  print build output
+```
+
+### Example
+
+```
+k6build  -c catalog.json  -p linux/amd64 -k v0.50.0 \ 
+   -d k6/x/kubernetes -d k6/x/output-kafka:v0.7.0311b995
+{
+  "id": "da39a3ee5e6b4b0d3255bfef95601890afd80709",
+  "url": "file:///tmp/buildcache3827025833/3449312261",
+  "Dependencies": [
+    {
+      "path": "github.com/grafana/xk6-kubernetes",
+      "vesion": "v0.8.0"
+    },
+    {
+      "path": "github.com/grafana/xk6-output-kafka",
+      "vesion": "v0.7.0"
+    },
+    {
+      "path": "go.k6.io/k6",
+      "vesion": "v0.50.0"
+    }
+  ],
+  "Checksum": "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855"
+}
+```
--- cmd/cmd.go
+++ cmd/cmd.go
@@ -2,8 +2,16 @@
 package cmd
 
 import (
+	"context"
+	"encoding/json"
 	"errors"
+	"fmt"
+	"os"
+	"strings"
 
+	"github.com/grafana/k6build"
+	"github.com/grafana/k6catalog"
+	"github.com/grafana/k6foundry"
 	"github.com/spf13/cobra"
 )
 
@@ -17,6 +25,14 @@ k6 build service returns artifacts that satisfies certain dependencies
 
 // New creates new cobra command for resolve command.
 func New() *cobra.Command {
+	var (
+		deps         []string
+		k6Constrains string
+		platform     string
+		registry     string
+		opts         k6foundry.NativeBuilderOpts
+	)
+
 	cmd := &cobra.Command{
 		Use:   "k6build",
 		Short: "k6 build service",
@@ -26,9 +42,53 @@ func New() *cobra.Command {
 		// this is needed to prevent cobra to print errors reported by subcommands in the stderr
 		SilenceErrors: true,
 		RunE: func(cmd *cobra.Command, _ []string) error {
+			catalog, err := k6catalog.NewCatalogFromJSON(registry)
+			if err != nil {
+				return fmt.Errorf("loading catalog %w", err)
+			}
+
+			// This is required to pass environment variables like the github access credential
+			opts.CopyEnv = true
+			builder, err := k6foundry.NewNativeBuilder(context.TODO(), opts)
+			if err != nil {
+				return fmt.Errorf("setting up builder %w", err)
+			}
+
+			cache, err := k6build.NewTempFileCache()
+			if err != nil {
+				return fmt.Errorf("creating build cache %w", err)
+			}
+
+			srv := k6build.NewBuildService(catalog, builder, cache)
+
+			buildDeps := []k6build.Dependency{}
+			for _, d := range deps {
+				name, constrains, _ := strings.Cut(d, ":")
+				if constrains == "" {
+					constrains = "*"
+				}
+				buildDeps = append(buildDeps, k6build.Dependency{Name: name, Constraints: constrains})
+			}
+
+			artifact, err := srv.Build(context.TODO(), platform, k6Constrains, buildDeps)
+			if err != nil {
+				return fmt.Errorf("building %w", err)
+			}
+
+			encoder := json.NewEncoder(os.Stdout)
+			encoder.SetIndent("", "  ")
+			encoder.Encode(artifact)
+
 			return nil
 		},
 	}
+
+	cmd.Flags().StringArrayVarP(&deps, "dependency", "d", nil, "list of dependencies in form package:constrains")
+	cmd.Flags().StringVarP(&k6Constrains, "k6-constrains", "k", "*", "k6 version constrains")
+	cmd.Flags().StringVarP(&platform, "platform", "p", "", "target platform (default GOOS/GOARCH)")
+	cmd.MarkFlagRequired("platform")
+	cmd.Flags().StringVarP(&registry, "catalog", "c", "catalog.json", "dependencies catalog")
+	cmd.Flags().BoolVarP(&opts.Verbose, "verbose", "v", false, "print build output")
 
 	return cmd
 }
