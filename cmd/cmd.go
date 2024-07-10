8c68b0abbb9850eb51a55d735906f2bba28f76b0
Iván SZKIBA <szkiba@users.noreply.github.com>
2024-07-10 10:59:08 +0200

    docs: Add minimal usage description to the root command.

--- cmd/cmd.go
+++ cmd/cmd.go
@@ -5,7 +5,16 @@ import "github.com/spf13/cobra"
 
 // New creates a new root command for k6build
 func New() *cobra.Command {
-	root := &cobra.Command{}
+	root := &cobra.Command{
+		Use:               "k6build",
+		Short:             "Build k6 with various builders.",
+		Long:              "Build k6 using one of the supported builders.",
+		SilenceUsage:      true,
+		SilenceErrors:     true,
+		DisableAutoGenTag: true,
+		CompletionOptions: cobra.CompletionOptions{DisableDefaultCmd: true},
+	}
+
 	root.AddCommand(NewLocal())
 	root.AddCommand(NewServer())
 	root.AddCommand(NewClient())
