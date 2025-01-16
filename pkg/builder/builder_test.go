685e6d83bc688cff8e2f1000da6d1efcc1ff1044
Pablo Chacin <pablochacin@gmail.com>
2025-01-16 19:52:17 +0100

    move builder test to its package
    Signed-off-by: Pablo Chacin <pablochacin@gmail.com>

--- pkg/builder/builder_test.go
+++ pkg/builder/builder_test.go
@@ -1,17 +1,98 @@
-package local
+package builder
 
 import (
 	"context"
 	"errors"
+	"fmt"
+	"net/http/httptest"
 	"sync"
 	"testing"
 
 	"github.com/grafana/k6build"
 	"github.com/grafana/k6build/pkg/catalog"
+	"github.com/grafana/k6build/pkg/store/file"
+	"github.com/grafana/k6foundry"
+	"github.com/grafana/k6foundry/pkg/testutils/goproxy"
 
 	"github.com/google/go-cmp/cmp"
 	"github.com/google/go-cmp/cmp/cmpopts"
 )
+
+// DependencyComp compares two dependencies for ordering
+func DependencyComp(a, b catalog.Module) bool { return a.Path < b.Path }
+
+// SetupTestBuilder setups a local build service for testing
+func SetupTestBuilder(t *testing.T) (*Builder, error) {
+	modules := []struct {
+		path    string
+		version string
+		source  string
+	}{
+		{
+			path:    "go.k6.io/k6",
+			version: "v0.1.0",
+			source:  "testdata/deps/k6",
+		},
+		{
+			path:    "go.k6.io/k6",
+			version: "v0.2.0",
+			source:  "testdata/deps/k6",
+		},
+		{
+			path:    "go.k6.io/k6ext",
+			version: "v0.1.0",
+			source:  "testdata/deps/k6ext",
+		},
+		{
+			path:    "go.k6.io/k6ext",
+			version: "v0.2.0",
+			source:  "testdata/deps/k6ext",
+		},
+		{
+			path:    "go.k6.io/k6ext2",
+			version: "v0.1.0",
+			source:  "testdata/deps/k6ext2",
+		},
+	}
+
+	// creates a goproxy that serves the given modules
+	proxy := goproxy.NewGoProxy()
+	for _, m := range modules {
+		err := proxy.AddModVersion(m.path, m.version, m.source)
+		if err != nil {
+			return nil, fmt.Errorf("setup %w", err)
+		}
+	}
+
+	goproxySrv := httptest.NewServer(proxy)
+
+	catalog, err := catalog.NewCatalogFromFile("testdata/catalog.json")
+	if err != nil {
+		return nil, fmt.Errorf("setting up test builder %w", err)
+	}
+
+	store, err := file.NewFileStore(t.TempDir())
+	if err != nil {
+		return nil, fmt.Errorf("creating temporary object store %w", err)
+	}
+
+	return New(context.Background(), Config{
+		Opts: Opts{
+			GoOpts: k6foundry.GoOpts{
+				CopyGoEnv: true,
+				Env: map[string]string{
+					"GOPROXY":   goproxySrv.URL,
+					"GONOPROXY": "none",
+					"GOPRIVATE": "go.k6.io",
+					"GONOSUMDB": "go.k6.io",
+				},
+				TmpCache: true,
+			},
+		},
+		Catalog: catalog,
+		Store:   store,
+	})
+}
 
 func TestDependencyResolution(t *testing.T) {
 	t.Parallel()
@@ -72,7 +153,7 @@ func TestDependencyResolution(t *testing.T) {
 		t.Run(tc.title, func(t *testing.T) {
 			t.Parallel()
 
-			buildsrv, err := SetupTestLocalBuildService(t)
+			buildsrv, err := SetupTestBuilder(t)
 			if err != nil {
 				t.Fatalf("test setup %v", err)
 			}
@@ -103,7 +184,7 @@ func TestDependencyResolution(t *testing.T) {
 
 func TestIdempotentBuild(t *testing.T) {
 	t.Parallel()
-	buildsrv, err := SetupTestLocalBuildService(t)
+	buildsrv, err := SetupTestBuilder(t)
 	if err != nil {
 		t.Fatalf("test setup %v", err)
 	}
@@ -250,7 +331,7 @@ func TestIdempotentBuild(t *testing.T) {
 // Attempting to write the same artifact twice will return an error.
 func TestConcurrentBuilds(t *testing.T) {
 	t.Parallel()
-	buildsrv, err := SetupTestLocalBuildService(t)
+	buildsrv, err := SetupTestBuilder(t)
 	if err != nil {
 		t.Fatalf("test setup %v", err)
 	}
