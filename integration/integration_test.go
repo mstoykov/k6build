8f3b6f1f6bae68803905b3fa3ea87183d50b9cfb
Pablo Chacin <pablochacin@gmail.com>
2025-01-24 13:58:30 +0100

    refactor test env setup into testutils package
    Signed-off-by: Pablo Chacin <pablochacin@gmail.com>

--- integration/integration_test.go
+++ integration/integration_test.go
@@ -6,69 +6,24 @@ package k6provider
 import (
 	"context"
 	"fmt"
-	"net/http/httptest"
 	"path/filepath"
 	"runtime"
 	"testing"
 
 	"github.com/grafana/k6build"
-	"github.com/grafana/k6build/pkg/builder"
-	"github.com/grafana/k6build/pkg/catalog"
 	"github.com/grafana/k6build/pkg/client"
-	"github.com/grafana/k6build/pkg/server"
-	strclient "github.com/grafana/k6build/pkg/store/client"
-	filestore "github.com/grafana/k6build/pkg/store/file"
-	storesrv "github.com/grafana/k6build/pkg/store/server"
+	"github.com/grafana/k6build/pkg/testutils"
 	"github.com/grafana/k6build/pkg/util"
 )
 
 func Test_BuildServer(t *testing.T) {
 	t.Parallel()
 
-	// 1. setup a file store
-	store, err := filestore.NewFileStore(filepath.Join(t.TempDir(), "store"))
+	testEnv, err := testutils.NewTestEnv(testutils.TestEnvConfig{WorkDir: t.TempDir()})
 	if err != nil {
-		t.Fatalf("store setup %v", err)
+		t.Fatalf("test env setup %v", err)
 	}
-	storeConfig := storesrv.StoreServerConfig{
-		Store: store,
-	}
-
-	// 2. start an object store server
-	storeHandler, err := storesrv.NewStoreServer(storeConfig)
-	if err != nil {
-		t.Fatalf("store setup %v", err)
-	}
-	storeSrv := httptest.NewServer(storeHandler)
-
-	// 3. configure a builder
-	storeClient, err := strclient.NewStoreClient(strclient.StoreClientConfig{Server: storeSrv.URL})
-	if err != nil {
-		t.Fatalf("store client setup %v", err)
-	}
-	catalog, err := catalog.DefaultCatalog()
-	if err != nil {
-		t.Fatalf("build server setup %v", err)
-	}
-	buildConfig := builder.Config{
-		Opts: builder.Opts{
-			GoOpts: builder.GoOpts{
-				CopyGoEnv: true,
-			},
-		},
-		Catalog: catalog,
-		Store:   storeClient,
-	}
-	builder, err := builder.New(context.TODO(), buildConfig)
-	if err != nil {
-		t.Fatalf("setup %v", err)
-	}
-
-	// 4. start a builder server
-	srvConfig := server.APIServerConfig{
-		BuildService: builder,
-	}
-	buildSrv := httptest.NewServer(server.NewAPIServer(srvConfig))
+	t.Cleanup(testEnv.Cleanup)
 
 	// 5. test building k6 with different options
 	testCases := []struct {
@@ -88,7 +43,7 @@ func Test_BuildServer(t *testing.T) {
 		t.Run(tc.title, func(t *testing.T) {
 			client, err := client.NewBuildServiceClient(
 				client.BuildServiceClientConfig{
-					URL: buildSrv.URL,
+					URL: testEnv.BuildServiceURL(),
 				},
 			)
 			if err != nil {
